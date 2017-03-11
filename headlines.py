dd52daccd965ff4016641a91aaa94792871f819b
Kevin Nguyen <kvn219@nyu.edu>
2017-03-11 12:07:57 -0500

    add dynamic data

--- headlines.py
+++ headlines.py
@@ -3,10 +3,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
-             'cnn': 'http://rss.cnn.com/rss/edition.rss',
-             'fox': 'http://feeds.foxnews.com/foxnews/latest',
-             'iol': 'http://www.iol.co.za/cmlink/1.640',
+RSS_FEEDS = {'bbc'            : 'http://feeds.bbci.co.uk/news/rss.xml',
+             'cnn'            : 'http://rss.cnn.com/rss/edition.rss',
+             'fox'            : 'http://feeds.foxnews.com/foxnews/latest',
+             'iol'            : 'http://www.iol.co.za/cmlink/1.640',
              'fivethirtyeight': 'https://fivethirtyeight.com/all/feed'}
 
 
@@ -15,7 +15,8 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 def get_news(publication="bbc"):
     feed = feedparser.parse(RSS_FEEDS[publication])
     first_article = feed['entries'][0]
-    return render_template("home.html", article=first_article)
+    return render_template("home.html", title=first_article.get("title"), published=first_article.get("published"),
+                           summary=first_article.get("summary"))
 
 
 if __name__ == '__main__':
